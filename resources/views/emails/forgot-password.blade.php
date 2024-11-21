80be953e5abd525100fc1821e99d0eb99ef97d0a
Thi Tinh <thitrungtinh@gmail.com>
2024-11-21 16:08:39 +0700

    sửa lại view forgot-password

--- resources/views/emails/forgot-password.blade.php
+++ resources/views/emails/forgot-password.blade.php
@@ -1,11 +1,21 @@
-<div style="border: 3px solid green; padding: 15px ; background: lightgreen; width: 600px; margin: auto">
-    <h3>Xin chào, {{ $user->name }}</h3>
+<div
+    style="border: 3px solid green; padding: 15px; background: lightgreen; width: 600px; margin: auto; border-radius: 10px; font-family: Arial, sans-serif; color: #064420;">
+    <h3 style="text-align: center;">Xin chào, {{ $user->name }}</h3>
 
     <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad sunt, quos alias saepe quod temporibus! Molestiae libero, quam quae dignissimos eaque aperiam minus, enim impedit, dicta facere aut nam voluptatem!
+        Chào bạn, chúng tôi rất vui khi được hỗ trợ bạn! Dưới đây là thông tin bạn cần:
     </p>
 
-    <p>
-        OTP của bạn: {{ $otp }}
+    <p style="font-size: 18px; font-weight: bold; text-align: center;">
+        Mã OTP của bạn: <span style="color: #d9534f;">{{ $otp }}</span>
+    </p>
+
+    <p style="text-align: center;">
+        Mã OTP này có hiệu lực trong vòng 1 phút. Vui lòng không chia sẻ mã này với bất kỳ ai để đảm bảo an toàn cho tài
+        khoản của bạn.
+    </p>
+
+    <p style="text-align: right; font-style: italic;">
+        Trân trọng,<br> Đội ngũ hỗ trợ
     </p>
 </div>
